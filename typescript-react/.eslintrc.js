bd3650524ce86dfde6ca8124331cfce9044e3709
Justin <57725347+juskek@users.noreply.github.com>
2023-10-31 18:08:22 +0000

    feat: add typescript-react linting example

---
+++ typescript-react/.eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+    "env": {
+      "browser": true,
+      "node": true
+    },
+    "settings": {
+      "react": {
+        "version": "detect"
+      }
+    },
+    extends: [
+      "eslint:recommended",
+      "plugin:@typescript-eslint/recommended",
+      "plugin:react/recommended",
+      "plugin:react-hooks/recommended",
+    ],
+    plugins: [
+      "react",
+      "prettier",
+    ],
+    overrides: [
+      {
+        files: ["*.ts", "*.tsx", "*.d.ts"],
+        parserOptions: {
+          project: "./tsconfig.json",
+        },
+      },
+    ],
+    rules: {
+      "import/order": "off",
+      "react-hooks/exhaustive-deps": "error",
+    },
+  };
